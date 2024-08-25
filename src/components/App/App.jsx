3863c6c09909cf9f957aac361817969667a40f85
Vlad P <vlad@pysarenko.com>
2024-08-25 17:28:22 +0200

    fix1

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -6,7 +6,6 @@ import Loader from '../Loader/Loader';
 const HomePage = lazy(() => import('../../pages/HomePage/HomePage'));
 const MoviesPage = lazy(() => import('../../pages/MoviesPage/MoviesPage'));
 const MovieDetailsPage = lazy(() => import('../../pages/MovieDetailsPage/MovieDetailsPage'));
-const MovieDetails = lazy(() => import('../MovieDetails/MovieDetails'));
 const MovieCast = lazy(() => import('../MovieCast/MovieCast'));
 const MovieReviews = lazy(() => import('../MovieReviews/MovieReviews'));
 
@@ -18,7 +17,6 @@ export default function App() {
           <Route index element={<HomePage />} />
           <Route path="/movies" element={<MoviesPage />} />
           <Route path="/movies/:movieId" element={<MovieDetailsPage />}>
-            <Route index element={<MovieDetails />} />
             <Route path="cast" element={<MovieCast />} />
             <Route path="reviews" element={<MovieReviews />} />
           </Route>
